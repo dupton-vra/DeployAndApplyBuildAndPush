f13439e66ba7460eab01c8720d2c892323da80d5
dupton-vra <67922920+dupton-vra@users.noreply.github.com>
2021-03-03 12:01:45 -0500

    Update git.sh

--- git.sh
+++ git.sh
@@ -1,13 +1,12 @@
-
+rm -rf *
 docker login -u dgupton75 -p Vcdexswzaq1!
 git clone  https://github.com/dupton-vra/node-bulletin-board2.git
 cd node-bulletin-board2
 docker build --no-cache --tag bulletinboard:1.$version .
 docker run --publish 8000:8080 --detach --name bb$version bulletinboard:1.$version
-sleep 2
-curl http://localhost:8000
 
-docker tag bulletinboard:1.$1 dgupton75/bulletinboard:1.$version
+
+docker tag bulletinboard:1.$version dgupton75/bulletinboard:1.$version
 docker push dgupton75/bulletinboard:1.$version
 docker rm --force bb$version
 rm -rf *
